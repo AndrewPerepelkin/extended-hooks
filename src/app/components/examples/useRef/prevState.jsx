9fb68b8bcd58f25a5f463979db7ae64b4b3936f1
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-08 16:06:13 +0300

    feat: added example of capture  previous state

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+
 const PrevStateExample = () => {
+    const previousState = useRef("");
+    const [otherState, setOtherState] = useState("false");
+
+    const toggleOtherStateHandler = () => {
+        setOtherState((prevState) =>
+            prevState === "false" ? "true" : "false"
+        );
+    };
+
+    useEffect(() => {
+        previousState.current = otherState;
+    }, [otherState]);
+
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <p>Предыдущее состояние: {previousState.current}</p>
+            <p>Текущее состояние: {otherState}</p>
+            <button
+                className="btn btn-primary"
+                onClick={toggleOtherStateHandler}
+            >
+                Toggle Other State
+            </button>
         </CardWrapper>
     );
 };
