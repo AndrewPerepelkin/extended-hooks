6207fd7fa1e5a7535067d711bbb4c3be843ad9bb
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-08 15:48:34 +0300

    feat: added render counter example

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
+    const renderCount = useRef(0);
+    const [otherState, setOtherState] = useState(false);
+
+    const toggleOtherStateHandler = () => {
+        setOtherState(!otherState);
+    };
+
+    useEffect(() => {
+        renderCount.current++;
+    });
+
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+            <p>Количество рендеров: {renderCount.current}</p>
+            <button
+                className="btn btn-primary"
+                onClick={toggleOtherStateHandler}
+            >
+                Toggle Other State
+            </button>
         </CardWrapper>
     );
 };
